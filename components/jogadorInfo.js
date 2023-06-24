6d48a7ef4b1e931de5b3d27487e09b98e20416d2
Kurokishin <rafaelpereirasouza15@gmail.com>
2023-06-24 18:45:08 -0300

    feat: adiciona novas informações

--- components/jogadorInfo.js
+++ components/jogadorInfo.js
@@ -1,24 +1,88 @@
+import React, { useEffect, useState } from 'react';
+import { View, Text, Image } from 'react-native';
 import axios from 'axios';
 
-// Função para buscar jogadores e seus respectivos times
-const GetPlayerInfo = async (searchPlayer) => {
-  try {
-    const apiKey = '3e0a856bd02675eba4ca1a26f3c5a98dbc1b1118c44f1dac567623de4af6e24a';
-    const apiUrl = `https://apiv3.apifootball.com/?action=get_players&player_name=${searchPlayer}&APIkey=${apiKey}`;
-    
-    const response = await axios.get(apiUrl);
+const GetPlayerInfo = ({ searchQuery }) => {
+  const [players, setPlayers] = useState([]);
 
-    // Extrair os campos de nome do jogador e time da resposta da API
-    const players = response.data.map((player) => ({
-      nome: player.player_name,
-      time: player.team_name,
-    }));
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const apiKey = '3e0a856bd02675eba4ca1a26f3c5a98dbc1b1118c44f1dac567623de4af6e24a';
+        const apiUrl = `https://apiv3.apifootball.com/?action=get_players&player_name=${searchQuery}&APIkey=${apiKey}`;
 
-    return players;
-  } catch (error) {
-    console.error('Error searching players:', error);
-    throw error;
-  }
+        const response = await axios.get(apiUrl);
+        const filteredPlayers = response.data.map(player => ({
+          nome: player.player_name,
+          time: player.team_name,
+          foto: player.player_image,
+          nacionalidade: player.player_country,
+          qtdGolsMarcados: player.player_goals,
+          posicao: player.player_type
+        }));
+        setPlayers(filteredPlayers);
+      } catch (error) {
+        console.error('Error fetching player data:', error);
+      }
+    };
+
+    if (searchQuery) {
+      fetchData();
+    } else {
+      setPlayers([]);
+    }
+  }, [searchQuery]);
+
+  return (
+    <View style={styles.playerInfo}>
+      {players.map(player => (
+        <View key={player.nome} style={styles.playerCard}>
+          <Text>Jogador</Text>
+          <Text style={styles.playerName}>Nome: {player.nome}</Text>
+          <Text>Time: {player.time}</Text>
+          {player.foto && <Image source={{ uri: player.foto }} style={styles.playerImage} />}
+          <Text>Posição: {player.posicao}</Text>
+          <Text>Nacionalidade: {player.nacionalidade}</Text>
+          <Text>Gols marcados: {player.qtdGolsMarcados}</Text>
+        </View>
+      ))}
+    </View>
+  );
 };
 
 export default GetPlayerInfo;
+
+const styles = {
+  playerInfo: {
+    flex: 1,
+    // Add any other styles for the player info container
+  },
+  playerCard: {
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    padding: 20,
+    borderRadius: 10,
+    marginBottom: 10,
+    elevation: 5,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 4,
+    borderColor: '#000',
+    borderWidth: 1,
+  },
+  playerName: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginTop: 10,
+  },
+  playerImage: {
+    width: 200,
+    height: 200,
+    marginTop: 10,
+    borderRadius: 10,
+  },
+};
