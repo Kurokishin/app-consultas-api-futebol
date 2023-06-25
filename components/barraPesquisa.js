9422a5e056055de0be507eeaf8ef95e45bf790f8
Kurokishin <rafaelpereirasouza15@gmail.com>
2023-06-24 21:40:14 -0300

    refactor: estilização do botão de pesquisa

--- components/barraPesquisa.js
+++ components/barraPesquisa.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, TextInput, Button } from 'react-native';
+import { Text, View, TextInput, StyleSheet, TouchableOpacity } from 'react-native';
 
 const SearchBar = ({ onSearch }) => {
   const [playerName, setPlayerName] = useState('');
@@ -13,27 +13,43 @@ const SearchBar = ({ onSearch }) => {
   };
 
   return (
-    <View>
+    <View style={styles.container}>
       <TextInput
         style={styles.barraPesquisa}
         value={playerName}
         onChangeText={handleInputChange}
       />
-      <Button title="Pesquisar" onPress={handleSearch} />
+      <TouchableOpacity style={styles.botao} onPress={handleSearch}>
+        <Text style={styles.botaoTexto}>Pesquisar</Text>
+      </TouchableOpacity>
     </View>
   );
 };
 
 export default SearchBar;
 
-const styles = {
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
   barraPesquisa: {
+    flex: 1,
     padding: 10,
     fontSize: 16,
     borderRadius: 4,
     borderWidth: 1,
     borderColor: '#ccc',
     marginRight: 10,
-    width: 300,
   },
-};
+  botao: {
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 4,
+    backgroundColor: '#0066cc',
+  },
+  botaoTexto: {
+    fontSize: 16,
+    color: '#fff',
+  },
+});
